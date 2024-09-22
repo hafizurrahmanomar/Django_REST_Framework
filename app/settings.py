228e200a330d4d191839f0432c88ed7f898970ce
Hafizur Rahman Omar <hafizurrahmanomar@gmail.com>
2024-09-22 23:24:56 +0600

    part-02

--- app/settings.py
+++ app/settings.py
@@ -70,6 +70,7 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mydb',
